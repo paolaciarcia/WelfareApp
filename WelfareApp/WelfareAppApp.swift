19dc30912a7a3a3b65ad0eb8df00e3794f300094
Paola Golombieski Ciarcia <paolaciarcia20@gmail.com>
2023-08-02 16:25:26 -0300

    Add project

---
+++ WelfareApp/WelfareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelfareAppApp.swift
+//  WelfareApp
+//
+//  Created by Paola Golombieski Ciarcia on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WelfareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
