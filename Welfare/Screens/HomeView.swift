c01a20e89c3310627be09afcc600bd0cec2ef2d6
Paola Golombieski Ciarcia <paolaciarcia20@gmail.com>
2023-08-03 15:30:48 -0300

    Add HomeView

--- Welfare/Screens/HomeView.swift
+++ Welfare/Screens/HomeView.swift
@@ -10,18 +10,49 @@ import SwiftUI
 struct HomeView: View {
     //UserDefaults and UserDefaults key
     @AppStorage("onboarding") var isOnboardingViewActive = false
+
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+        VStack {
+            Spacer()
+            VStack(spacing: 35) {
+                ZStack {
+                    Circle()
+                        .stroke(.gray.opacity(0.1), lineWidth: 40)
+                        .frame(width: 260, height: 260, alignment: .center)
+
+                    Circle()
+                        .stroke(.gray.opacity(0.1), lineWidth: 80)
+                        .frame(width: 260, height: 260, alignment: .center)
+
+                    Image("character-2")
+                        .resizable()
+                        .scaledToFit()
+                }
+
+                Text("The time that leads to mastery is \ndependent on the intensity of our focus")
+                    .multilineTextAlignment(.center)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.gray.opacity(0.8))
+            }
+
+            Spacer()
 
             Button {
                 isOnboardingViewActive = true
             } label: {
-                Text("Restart")
-            }
+                Image(systemName: "arrow.2.circlepath.circle.fill")
+                    .imageScale(.large)
 
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         }
+        .padding()
     }
 }
 
